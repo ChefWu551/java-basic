56e623250f99fc650f1cb95b03c36c233b39592d
wuyuefeng <wuyuefeng@thundersdata.com>
2021-03-08 21:47:13 +0800

    增加异常try finally执行顺序

---
+++ src/main/test/com/yuefeng/hashmap/Substring.java
@@ -0,0 +1,14 @@
+package com.yuefeng.hashmap;
+
+import org.junit.Test;
+
+import java.lang.annotation.Target;
+
+public class Substring {
+
+    @Test
+    public void subStringTest() {
+        String a = "0v3369a0";
+        System.out.println(a.substring(2, 6));
+    }
+}
