3fed778f6e68ad9cb887d3c27a5482e615d538c1
wuyuefeng <wuyuefeng@thundersdata.com>
2021-05-10 10:13:06 +0800

    '补充final学习笔记'

--- src/main/java/com/yuefeng/jvm/_03NonFinal.java
+++ src/main/java/com/yuefeng/jvm/_03NonFinal.java
@@ -1,7 +1,9 @@
 package com.yuefeng.jvm;
 
+import java.util.Random;
+
 /**
- * final类型的数据，在编译的时候就已经带着赋值信息了（constant value）,可查看字节码文件
+ * final类型的数据，在编译的时候就已经带着赋值信息了（Field-constant value）,可查看字节码文件
  * 非non-final类型的赋值，对于类变量来说，load阶段的prepare会进行初始化默认值，赋值阶段在clinit阶段，显式初始化
  */
 public class _03NonFinal {
@@ -16,4 +18,10 @@ public class _03NonFinal {
         System.out.println(a);
         System.out.println(b);
     }
+
+    public final String str = "str";
+
+    public final String strInstance = new String("strInstance");
+
+    public final int value = new Random().nextInt(10);
 }
